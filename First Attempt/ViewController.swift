2fc9da63b880cee40e0177287c3253134d628b4c
Oliver Boulton <olliemarkboulton@gmail.com>
2021-03-04 17:37:01 +0000

    Booleans

--- First Attempt/ViewController.swift
+++ First Attempt/ViewController.swift
@@ -8,33 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
     
     @IBAction func buttonTapped(_ sender: Any) {
-        print(text1.text!)
-        print(text2.text!)
-        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        let addition = true
         
-    
+        if addition {
+            theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+            
+        }else {
+            theLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+            
+        }
     }
-
+    
     
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
     
     
-  
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
+        
         
     }
-
-
+    
+    
 }
 
