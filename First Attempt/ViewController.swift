3c909b84af6767c78e15a2e784955ddc3da0f3e2
Oliver Boulton <olliemarkboulton@gmail.com>
2021-03-04 17:20:58 +0000

    Simple calculater edit

--- First Attempt/ViewController.swift
+++ First Attempt/ViewController.swift
@@ -11,27 +11,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
     
     @IBAction func buttonTapped(_ sender: Any) {
-        theLabel.text = "Hello my G"
-        print("PUSH ME Button Tapped")
+        print(text1.text!)
+        print(text2.text!)
+        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
         
-        tapCount = tapCount + 1
-        if tapCount >= 10 {
-            theLabel.text = "This process is working hold on"
-        }
-        print(tapCount)
-        
-        /* this is how you type multi line comments
-         see multiple lines*/
+    
     }
-    @IBAction func buttonTapped2(_ sender: Any) {
-        theLabel.text = "Seans a Nyoo"
-        print("Tap This Button too Button Tapped")
-        self.view.backgroundColor = UIColor.blue
-        // This is how you leave a comment in the code. this two//
-    }
+
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+  
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
